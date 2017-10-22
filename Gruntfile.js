e94539cd4c4f4ce43dc081ed535cfa7e6267c9ff
Lukasz Rozwadowski <lukasz.rozwadowski7@gmail.com>
2017-10-22 23:11:48 +0200

    gitignore added

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'main.css': 'main.sass'
+        }
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'images/build/'
+        }]
+      }
+    },
+    
+    watch: {
+    scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        },
+    }
+  }
+
+
+  });
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+  // Default task(s).
+
+  grunt.registerTask('default', ['sass', 'imagemin']);
+};
